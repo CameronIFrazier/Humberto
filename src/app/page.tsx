159a4522406f8cba6a5f777cd8002884475075c4
CameronIFrazier <taterfrazier1@gmail.com>
2026-01-09 14:56:22 -0800

    ulgier version but only on mobile, desktop unfinished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,18 @@ export default function Home() {
   //forcing to vercel
   return (
     <>
+    <section className=" w-full flex flex-row items-center justify-center pt-4 bg-white">
+          <div className="w-full flex flex-row items-center justify-center relative">
+            <Image
+              src={"/images/image.png"}
+              alt="Logo"
+              className="object-contain"
+              width={100} // adjust as needed
+              height={50} // adjust as needed
+            ></Image>
+            
+          </div>
+        </section>
       <section className="bg-white min-h-screen w-full flex flex-col md:hidden items-center ">
         {/* Header Scetion */}
         <section className="bg-gray-700 w-full flex flex-col items-center p-4 justify-center">
@@ -56,93 +68,8 @@ export default function Home() {
             </div>
           </div>
         </section>
-        {/* Logo Section begin */}
-        <section className=" w-full flex flex-row items-center justify-center pt-4">
-          <div className="w-[75%] flex flex-row items-center justify-between relative">
-            <Image
-              src={"/images/image.png"}
-              alt="Logo"
-              className="object-contain"
-              width={100} // adjust as needed
-              height={50} // adjust as needed
-            ></Image>
-            <Popover className="relative">
-              <PopoverButton className="p-2 rounded-md hover:bg-gray-100 focus:outline-none">
-                <Bars3Icon className="h-6 w-6 text-black" />
-              </PopoverButton>
-
-              {/* Popover panel */}
-              <PopoverPanel
-                anchor="bottom"
-                className="mt-2 flex flex-col rounded-lg bg-gray-100 p-4 shadow-lg"
-              >
-                <a
-                  onClick={() => {
-                    document
-                      .getElementById("#house-cleaning")
-                      ?.scrollIntoView({ behavior: "smooth" });
-                  }}
-                  className="px-2 py-2 rounded hover:bg-gray-200 text-black cursor-pointer"
-                >
-                  House Cleaning
-                </a>
-                <a
-                  onClick={() => {
-                    document
-                      .getElementById("#house-painting")
-                      ?.scrollIntoView({ behavior: "smooth" });
-                  }}
-                  className="px-2 py-2 rounded hover:bg-gray-200 text-black cursor-pointer"
-                >
-                  House Painting
-                </a>
-                <a
-                  onClick={() => {
-                    document
-                      .getElementById("#popcorn-ceiling")
-                      ?.scrollIntoView({ behavior: "smooth" });
-                  }}
-                  className="px-2 py-2 rounded hover:bg-gray-200 text-black cursor-pointer"
-                >
-                  Popcorn Ceiling Removal
-                </a>
-                <a
-                  onClick={() => {
-                    document
-                      .getElementById("#contact-form")
-                      ?.scrollIntoView({ behavior: "smooth" });
-                  }}
-                  className="px-2 py-2 rounded hover:bg-gray-200 text-black cursor-pointer"
-                >
-                  Email Us
-                </a>
-              </PopoverPanel>
-            </Popover>
-          </div>
-        </section>
-        {/* Logo Section end */}
-
-        {/* Content Section begin*/}
-        <section className="w-full bg mt-4 flex flex-col justify-center items-center">
-          {/* Hero Image with Text Overlay begin*/}
-          <div
-            className="h-64 w-full bg-center bg-cover bg-no-repeat flex flex-col items-center justify-center"
-            style={{ backgroundImage: "url('/images/low-lighting-room.jpg')" }}
-          >
-            <div className="flex flex-col p-4">
-              <p className=" text-white text-font-bold text-4xl font-bold">
-                Tired of your dirty House?<br></br>
-              </p>
-              <p className="">So are we.</p>
-            </div>
-          </div>
-          {/* Hero Image with Text Overlay end*/}
-          <div className="flex flex-col w-auto justify-center items-center ">
-            <header className="font-bold text-4xl text-gray-700 p-4 ">
-              Professional Home Remodeling and General Contractor Services
-            </header>
-          </div>
-        </section>
+        
+        
 
         {/*services section begin*/}
         <div className="relative h-64 w-full flex flex-col items-center justify-center">
@@ -163,22 +90,11 @@ export default function Home() {
             <p className="text-white text-4xl font-bold">
               House Cleaning Services
             </p>
-            <p className="text-white">
-              Keep your home sparkling clean with our professional cleaning
-              services. We handle every room with care, from kitchens to
-              bedrooms. Our team ensures a fresh and hygienic living space for
-              you and your family.
-            </p>
-            <button
-              onClick={() => {
-                document
-                  .getElementById("#contact-form")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-              className="inline-block px-2 py-1 rounded bg-gray-500 text-white"
-            >
-              Email Us
-            </button>
+            <header className="text-2xl ">Regular Cleanings</header>
+            <header className="text-2xl ">Deep Cleaning</header>
+            <header className="text-2xl ">Move In/Out</header>
+            <header className="text-2xl ">Post Construction</header>
+            
           </section>
         </div>
         <section
@@ -197,24 +113,21 @@ export default function Home() {
           {/* Content above overlay */}
           <div className="relative z-10 flex flex-col items-center justify-center gap-2 p-4 text-center">
             <p className="text-white text-4xl font-bold">
-              House Painting Services
+              Special Services
             </p>
-            <p className="text-white">
-              Transform your home with our expert interior and exterior painting
-              services. We use high-quality paints and techniques for a flawless
-              finish. Our work refreshes your space and adds lasting beauty to
-              every room.
-            </p>
-            <button
-              onClick={() => {
-                document
-                  .getElementById("#contact-form")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-              className="inline-block px-2 py-1 rounded bg-gray-500 text-white"
-            >
-              Email Us
-            </button>
+            <header className="text-2xl">Painting </header>
+                        <header className="text-2xl">Drywall </header>
+            
+
+            <header className="text-2xl">Flooring </header>
+            
+            <header className="text-2xl">Concrete </header>
+            
+
+            
+            
+            
+            
           </div>
         </section>
         <section
@@ -232,25 +145,12 @@ export default function Home() {
 
           {/* Content above overlay */}
           <div className="relative z-10 flex flex-col items-center justify-center gap-2 p-4 text-center">
-            <p className="text-white text-4xl font-bold">
-              Popcorn Ceiling Removal
-            </p>
-            <p className="text-white">
-              Update your home with our safe and efficient popcorn ceiling
-              removal services. We carefully remove outdated textures for a
-              smooth, modern ceiling. Enjoy a cleaner, brighter look that
-              enhances the overall feel of your home.
-            </p>
-            <button
-              onClick={() => {
-                document
-                  .getElementById("#contact-form")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-              className="inline-block px-2 py-1 rounded bg-gray-500 text-white"
-            >
-              Email Us
-            </button>
+            <header className="text-2xl fpont">Popcorn Ceiling Removal </header>
+            <header className="text-2xl">Decks and Porches </header>
+            <header className="text-2xl">Remodeling Kitchen </header>
+            <header className="text-2xl">Bath Remodeling </header>
+            <header className="text-2xl">Much More! </header>
+            
           </div>
         </section>
 
@@ -316,51 +216,17 @@ export default function Home() {
         {/* Gray bar section  */}
 
         <section className=" w-full flex flex-row items-center justify-center pt-4 gap-8 bg-gray-700 pb-4">
-          <header
-            onClick={() => {
-              document
-                .getElementById("house-cleaning")
-                ?.scrollIntoView({ behavior: "smooth" });
-            }}
-            className="text-white font cursor-pointer"
-          >
-            House Cleaning Services
-          </header>
-          <header className="text-white font">
-            Construction/Remodeling Servies{" "}
-          </header>
+          
           <header className="text-orange-300 font">We serve the Inland Empire, Orange County, and Los Angeles areas </header>
           
         </section>
 
         {/* Gray bar Section end */}
 {/* Hero Image with Text Overlay begin*/}
-        <section className="w-full bg mflex flex-col justify-center items-center">
-          
-          <div
-            className="h-210 w-full bg-center bg-cover bg-no-repeat flex flex-col items-center justify-center"
-            style={{ backgroundImage: "url('/images/low-lighting-room.jpg')" }}
-          >
-            <div className="flex flex-col p-4">
-              <p className=" text-white text-font-bold text-8xl font-bold">
-                Want your house this clean?
-                <br></br> So do we.<br></br>
-              </p>
-             
-            </div>
-            
-          </div>
-          
-          {/* Hero Image with Text Overlay end*/}
-          <div className="flex flex-col w-auto justify-center items-center ">
-            <header className="font-bold text-4xl text-gray-700 p-4 ">
-              Professional Home Remodeling and General Contractor Services
-            </header>
-          </div>
-        </section>
+        
 
         {/*services section begin*/}
-        <section className="grid grid-cols-3 justify-center center-tems">
+        <section className="grid grid-cols-2 justify-center center-tems w-[80%]">
         <section className="relative h-125 w-full flex flex-col items-center justify-center">
           {/* Background image */}
           <div
@@ -385,54 +251,10 @@ export default function Home() {
               bedrooms. Our team ensures a fresh and hygienic living space for
               you and your family.
             </p>
-            <button
-              onClick={() => {
-                document
-                  .getElementById("contact-form")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-              className="inline-block px-2 py-1 rounded bg-gray-500 text-white"
-            >
-              Email Us
-            </button>
+            
           </section>
         </section>
-        <section
-          id="house-painting"
-          className="relative h-125 w-full flex flex-col items-center justify-center"
-        >
-          {/* Background image */}
-          <div
-            className="absolute inset-0 bg-center bg-cover bg-no-repeat"
-            style={{ backgroundImage: "url('/images/painted-house.jpg')" }}
-          />
-
-          {/* Dark overlay */}
-          <div className="absolute inset-0 bg-black/50" />
-
-          {/* Content above overlay */}
-          <div className="relative z-10 flex flex-col items-center justify-center gap-2 p-4 text-center">
-            <p className="text-white text-4xl font-bold">
-              House Painting Services
-            </p>
-            <p className="text-white">
-              Transform your home with our expert interior and exterior painting
-              services. We use high-quality paints and techniques for a flawless
-              finish. Our work refreshes your space and adds lasting beauty to
-              every room.
-            </p>
-            <button
-              onClick={() => {
-                document
-                  .getElementById("contact-form")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-              className="inline-block px-2 py-1 rounded bg-gray-500 text-white"
-            >
-              Email Us
-            </button>
-          </div>
-        </section>
+        
         <section
           id="#popcorn-ceiling"
           className="relative h-125 w-full flex flex-col items-center justify-center"
@@ -457,16 +279,7 @@ export default function Home() {
               smooth, modern ceiling. Enjoy a cleaner, brighter look that
               enhances the overall feel of your home.
             </p>
-            <button
-              onClick={() => {
-                document
-                  .getElementById("contact-form")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-              className="inline-block px-2 py-1 rounded bg-gray-500 text-white"
-            >
-              Email Us
-            </button>
+            
           </div>
         </section>
         </section>
